a02a24ee1e5ed8e2efd06dcbe5a9a4ab60d5ade4
Gordon McLellan <gordonthree@gmail.com>
2017-11-10 11:45:28 -0500

    fixed typo

--- src/pca9633.cpp
+++ src/pca9633.cpp
@@ -60,7 +60,7 @@ static uint8_t _i2c_read(uint8_t address, uint8_t cmd) {
   return result;
 }
 
-statuc uint8_t linearize(uint8_t pwm) {
+uint8_t linearize(uint8_t pwm) {
   uint8_t result = pgm_read_byte(ledLinear + pwm);
   return result;
 }
